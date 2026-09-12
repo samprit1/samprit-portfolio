61bfbb980b84c35706e16f9d3476d2477b060613
samprit1 <samprit2006.sdm@gmail.com>
2026-09-12 23:20:31 +0530

    Add portfolio page

---
+++ src/app/motion-design/page.tsx
@@ -0,0 +1,1 @@
+import { CategoryPage } from '@/components/CategoryPage'; import { getPortfolioProjects } from '@/sanity/projects'; export default async function(){const projects=await getPortfolioProjects();return <CategoryPage type="motion-design" projects={projects.filter(p=>p.type==='motion-design')}/>}
