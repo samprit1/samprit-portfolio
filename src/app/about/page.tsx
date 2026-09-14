ae999b4724fa02ca7bdd1b34c22e5c0f0c98f132
samprit1 <samprit2006.sdm@gmail.com>
2026-09-14 19:42:56 +0530

    Connect About CTA to Contact toggle

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,3 @@
 import { getSiteSettings } from '@/sanity/projects'
 import { AboutExperience } from '@/components/AboutExperience'
-export default async function About(){const settings=await getSiteSettings();return <div className="mx-auto w-full max-w-[1500px] pt-4"><AboutExperience title={settings.aboutTitle} body={settings.aboutBody} aboutImageUrl={settings.aboutImageUrl || settings.profileImageUrl}/></div>}
+export default async function About(){const settings=await getSiteSettings();return <div className="mx-auto w-full max-w-[1500px] pt-4"><AboutExperience title={settings.aboutTitle} body={settings.aboutBody} aboutImageUrl={settings.aboutImageUrl || settings.profileImageUrl} contactEnabled={settings.contactEnabled}/></div>}
